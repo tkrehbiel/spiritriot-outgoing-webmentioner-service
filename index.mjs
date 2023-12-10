392c5ec998699c6c84518e9a6cc737aa89219b7b
Thomas Krehbiel <tkrehbiel@gmail.com>
2023-12-10 14:50:47 -0500

    delete handled messages

--- index.mjs
+++ index.mjs
@@ -1,13 +1,23 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({ path: './.env.local' });
+dotenv.config({ path: "./.env.local" });
+
+async function deleteMessage(queueName, receipt) {
+  if (!queueName) return;
+  const command = new DeleteMessageCommand({
+    QueueUrl: queueName,
+    ReceiptHandle: receipt,
+  });
+  return sqsClient.send(command);
+}
 
 async function process(message) {
-  console.log('outgoing webmentions for:', JSON.stringify(message));
+  console.log("outgoing webmentions for:", JSON.stringify(message, body));
+  deleteMessage(process.env.INCOMING_QUEUE, message.receiptHandle);
 }
 
 export async function handler(event, context) {
   for (const message of event.Records) {
-    await process(message.body);
+    await process(message);
   }
 }
